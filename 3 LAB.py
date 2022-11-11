48cfbddcf898f1c4cf75e8e41491ee912a6fa29f
BayramXojanov <114719655+BayramXojanov@users.noreply.github.com>
2022-11-11 09:26:03 +0500

    Add files via upload

---
+++ 3 LAB.py
@@ -0,0 +1,9 @@
+a=int(input('putin san kiritin: '))
+b=int(input('putin san kiritin: '))
+c=int(input('putin san kiritin: '))
+a%=10
+b%=10
+c%=10
+print('a saninin aqirgi cifri',a)
+print('b saninin aqirgi cifri',b)
+print('c saninin aqirgi cifri',c)
